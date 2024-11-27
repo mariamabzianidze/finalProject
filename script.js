ccfccb91716e9a4924b7f3e1a1301d25593934fa
Mariam Abzianidze <abzianidz3@gmail.com>
2024-11-27 21:44:42 +0400

    CSRPT

---
+++ script.js
@@ -0,0 +1,15 @@
+let burg = document.querySelector(".burger");
+let X = document.querySelector(".x_mark");
+let mob = document.querySelector(".header_nav");
+// burgerclick
+burg.addEventListener("click", () => {
+  mob.style.left = "0px";
+  X.style.display = "block";
+  burger.style.display = "none";
+});
+// xclick
+X.addEventListener("click", () => {
+  mob.style.left = "-100%";
+  X.style.display = "none";
+  burg.style.display = "block";
+});
